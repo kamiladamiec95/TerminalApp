d4d9e2591fedb8a9ecd2011d6489aecd852f77d3
Kamil <kamiladamiec95@wp.pl>
2023-02-08 22:54:17 +0100

    new features(touch, mv, pwd, rm, rmdir, zip)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import shutil
 
 def TerminalRun():
 
@@ -25,7 +26,34 @@ def TerminalRun():
             folder_name = ""
             for name in user_input_splitted[1:]:
                 folder_name = folder_name + name
-            os.mkdir(os.getcwd() + "/" + folder_name)    
+            os.mkdir(os.getcwd() + "/" + folder_name)
+
+        elif user_input_splitted[0] == "touch" and len(user_input_splitted) > 1:
+            pathlib.Path(os.getcwd() + "/" + user_input_splitted[1]).touch()
+
+        elif user_input_splitted[0] == "mv" and len(user_input_splitted) > 1:
+            pathlib.Path(os.getcwd() + "/" + user_input_splitted[1]).rename(os.getcwd() + "/" + user_input_splitted[2] + "/" + user_input_splitted[1])
+
+        elif user_input_splitted[0] == "pwd":
+            print(os.getcwd())
+
+        elif user_input_splitted[0] == "rm" and len(user_input_splitted) > 1:
+            file_name = user_input_splitted[1]
+            os.remove(file_name)
+
+        elif user_input_splitted[0] == "rmdir" and len(user_input_splitted) > 1:
+            folder_name = user_input_splitted[1]
+            if user_input_splitted[2] == "-r":
+                shutil.rmtree(folder_name)           
+            else:
+                os.rmdir(folder_name)
+
+        elif user_input_splitted[0] == "zip" and len(user_input_splitted) > 1:
+                file_to_zip = user_input_splitted[1]
+                shutil.make_archive(file_to_zip, "zip")
+
+        elif not user_input_splitted[0] == "quit":
+            print("Error")    
 
 
 if __name__ == "__main__":
